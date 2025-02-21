f349b69ce972d3faf963d35c02418b24938b6123
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2025-02-21 10:27:12 +0200

    update sqlite dialect

--- dialect.go
+++ dialect.go
@@ -15,20 +15,20 @@ func (a SQLiteDialect) CreateVersionTableQuery() string {
 		CREATE TABLE IF NOT EXISTS schema_version (
 			id INTEGER PRIMARY KEY CHECK (id = 0),
 			version INTEGER,
-			hash TEXT NOT NULL
+			checksum TEXT NOT NULL
 		);
-		INSERT INTO schema_version (id, version, hash) VALUES (0, 0, "") ON CONFLICT DO NOTHING;
+		INSERT INTO schema_version (id, version, checksum) VALUES (0, 0, "") ON CONFLICT DO NOTHING;
 	`
 }
 
 func (a SQLiteDialect) CurrentVersionQuery() string {
-	return `SELECT version, hash FROM schema_version;`
+	return `SELECT version, checksum FROM schema_version;`
 }
 
 func (a SQLiteDialect) SaveVersionQuery() string {
 	return `
 		UPDATE schema_version
-		SET version = $1, hash = $2
+		SET version = $1, checksum = $2
 		WHERE id = 0;
 	`
 }
